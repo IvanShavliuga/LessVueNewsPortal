38de562fdf0b065f6afec9f26e03e92d9fd26138
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-04-28 18:04:54 +0300

    vue.js version

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
 require('./css/normalize.css')
-require('./css/skeleton.css')
-require('./css/style.css')
-require('./images/robot2.jpg')
+require('./css/style.less')
+require('./index.js')
---
+++ src/appindex.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+div#appindex(data-id="appindex")
+  - var items = ['Главная','Наука','Технологии','Программирование','Тесты']
+  header
+    .logo IT
+      span NEWS
+    ul
+      each item in items
+        li=item
+  .promohead This is my work. New version 1.2.0. {{status}}
+  main 
+    each index in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+      apparticle 
+  footer  Это портфолио Ивана Иванова (Шавлюги) Использую flexbox и LESS
+
+</template>
+<script>
+import article from './article'
+export default {
+  data() {
+    return {
+      status:"200 OK",
+    }  
+  },
+  components: {
+    apparticle:article  
+  }
+}
+</script>
+<style>
+.color {
+  color: #98dba0;
+  font-size: 100px;
+}
+</style>
