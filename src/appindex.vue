6b38404ebf2827443f75cd94c1170c33c104defc
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-05-08 17:44:02 +0300

    header menu user

--- src/appindex.vue
+++ src/appindex.vue
@@ -1,12 +1,32 @@
 <template lang="pug">
 div#appindex(data-id="appindex")
   - var items = ['Главная','Наука','Технологии','Программирование','Тесты']
-  header
+  header.header
     .logo IT
       span NEWS
-    ul
+    ul.header__menu
       each item in items
         li=item
+    div.header__navbar
+      ul.header__nav
+        li.header__link
+          a(href="#" target="_blank") Profile
+        li.header__link
+          a(href="#" target="_blank") news
+        li.header__link
+          a(href="#" target="_blank") friends
+        li.header__link
+          a(href="#" target="_blank") groups
+        li.header__link
+          a(href="#" target="_blank") messages
+        li.header__link
+          a(href="#" target="_blank") comments
+        li.header__link
+          a(href="#" target="_blank") cards
+        li.header__link
+          a(href="#" target="_blank") settings
+        li.header__link
+          a(href="#" target="_blank") log out
   .promohead This is my work. New version 1.2.0. {{status}}
   main 
     appaside(:user="$store.getters.user", :friends="$store.getters.friends")
