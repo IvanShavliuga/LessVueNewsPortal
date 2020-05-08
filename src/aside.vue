170805d5f59d51bb37964461e3b080c0eedc4926
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-05-08 09:15:46 +0300

    Font aewsome

--- src/aside.vue
+++ src/aside.vue
@@ -3,6 +3,7 @@ aside.aside
   div.profile(:data-id="user.login+'_'+user.id")
     h4.profile__header  {{user.name}}
     p.profile__login 
+      i.fa.fa-id-badge.icon.icon__user 
       a(:href="user.link" target="_blank") {{user.login}}
     p.profile__info {{user.age}},  {{user.city}}
     p.profile__info {{user.spec}}
@@ -17,7 +18,11 @@ aside.aside
   div.contacts
     ul.contacts__list
       li.contacts__item(v-for="(c,key) in user.contacts" :key="key")
-        div.contacts__name {{c.messenger}}
+        div.contacts__name 
+          i(v-if="c.messenger=='phone'").fa.fa-phone.icon.icon__phone
+          i(v-if="c.messenger=='email'").fa.fa-envelope.icon.icon__email
+          i(v-if="c.messenger=='telegram'").fab.fa-telegram.icon.icon__telegram 
+          span {{c.messenger}}
         div.contacts__contact {{c.contact}}
 </template>
 <script>
