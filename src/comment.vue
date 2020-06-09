a8e05fa22d618f9784c47c89a3eb51ea8acb9fdb
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-06-09 10:08:43 +0300

    like & dislike comment

--- src/comment.vue
+++ src/comment.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
 div.comment
   h4.comment__user @{{user.login}}
-  p.comment__text {{comment.text}}
+  p.comment__text {{comment.text}} ({{comment.id}}, {{comment.likeclick}})
   p.comment__date 
     span {{comment.date}}
     span
-      i.far.fa-thumbs-up.icon.icon__vote(title="+")
+      i.far.fa-thumbs-up.icon.icon__vote(title="+", @click="likecomment(comment)")
       b {{comment.like}}
-      i.far.fa-thumbs-down.icon.icon__vote(title="-") 
+      i.far.fa-thumbs-down.icon.icon__vote(title="-", @click="dislikecomment(comment)") 
 </template>
 <script>
 export default  {
@@ -20,6 +20,14 @@ export default  {
       type:Object,
       required:true    
     }
+  },
+  methods: {
+    likecomment(comment) {
+      this.$store.dispatch("likecomment",comment);    
+    },
+    dislikecomment(comment) {
+      this.$store.dispatch("dislikecomment",comment);    
+    }  
   }
 }
 </script>
